169c7700e30bfc83a7fdc437feeda089361448df
Andreas Westerwick <westerwick@pconas.de>
2020-12-25 22:55:21 +0800

    move sample data

--- test/Spec.hs
+++ test/Spec.hs
@@ -42,12 +42,12 @@ ibm_place2 s = testCase s . either (fail . show) (void . pure) . parseLEF
 
 
 ibm01_lef, ibm02_lef, ibm07_lef, ibm08_lef, ibm09_lef, ibm10_lef, ibm11_lef, ibm12_lef :: Text
-ibm01_lef = decodeUtf8 $(embedFile "ibm-place2/ibm01/ibm01.lef")
-ibm02_lef = decodeUtf8 $(embedFile "ibm-place2/ibm02/ibm02-5l.lef")
-ibm07_lef = decodeUtf8 $(embedFile "ibm-place2/ibm07/ibm07-5l.lef")
-ibm08_lef = decodeUtf8 $(embedFile "ibm-place2/ibm08/ibm08-5l.lef")
-ibm09_lef = decodeUtf8 $(embedFile "ibm-place2/ibm09/ibm09-5l.lef")
-ibm10_lef = decodeUtf8 $(embedFile "ibm-place2/ibm10/ibm10-5l.lef")
-ibm11_lef = decodeUtf8 $(embedFile "ibm-place2/ibm11/ibm11-5l.lef")
-ibm12_lef = decodeUtf8 $(embedFile "ibm-place2/ibm12/ibm12-5l.lef")
+ibm01_lef = decodeUtf8 $(embedFile "sample/ibm-place2/ibm01/ibm01.lef")
+ibm02_lef = decodeUtf8 $(embedFile "sample/ibm-place2/ibm02/ibm02-5l.lef")
+ibm07_lef = decodeUtf8 $(embedFile "sample/ibm-place2/ibm07/ibm07-5l.lef")
+ibm08_lef = decodeUtf8 $(embedFile "sample/ibm-place2/ibm08/ibm08-5l.lef")
+ibm09_lef = decodeUtf8 $(embedFile "sample/ibm-place2/ibm09/ibm09-5l.lef")
+ibm10_lef = decodeUtf8 $(embedFile "sample/ibm-place2/ibm10/ibm10-5l.lef")
+ibm11_lef = decodeUtf8 $(embedFile "sample/ibm-place2/ibm11/ibm11-5l.lef")
+ibm12_lef = decodeUtf8 $(embedFile "sample/ibm-place2/ibm12/ibm12-5l.lef")
 
